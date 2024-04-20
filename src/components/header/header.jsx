9a6ad4a210d7a10c412fea9589efb48ac6454ce0
Nahuel Rosales <nahuelrosales@outlook.com>
2024-04-20 02:32:59 -0300

    modified header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,13 @@
 import { RiFacebookBoxFill, RiInstagramLine } from "react-icons/ri";
-
+import { GiHamburgerMenu } from "react-icons/gi";
 const Header = () => {
     return (
         <header className="flex items-center justify-between w-full py-2.5 text-lg fixed top-0 z-10 bg-primary opacity-80 text-white">
-            <div className="pl-16">
+            <div className="pl-3.5 lg:pl-16">
                 <h1 className="font-bold">Pura Vida Hospedaje</h1>
             </div>
-            <nav className="flex items-center px-4 mx-4 w-8/12 justify-evenly font-medium">
+<GiHamburgerMenu className="w-20 text-3xl"/>
+            <nav className="lg:flex hidden items-center px-4 mx-4 w-8/12 justify-evenly font-medium">
                 <h2>Habitaciones</h2>
                 <h2>Galería</h2>
                 <h2>Servicios</h2>
